3b5d206048b4e4e358511c8072984c4ac678435d
stratosphr <stratosphr@gmail.com>
2019-07-27 21:35:25 +0200

    Fresh start with RAM emulation

--- src/app/Emulator.tsx
+++ src/app/Emulator.tsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import './css/Emulator.css'
-import Screen from './core/Screen'
+import RAM from './core/RAM'
 
 interface IEmulatorProps {
 }
@@ -10,9 +10,16 @@ interface IEmulatorState {
 
 export default class Emulator extends Component<IEmulatorProps, IEmulatorState> {
 
+	private readonly ram: RAM
+
+	constructor(props: Readonly<IEmulatorProps>) {
+		super(props)
+		this.ram = new RAM(4096)
+	}
+
 	public render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
 		return (
-			<Screen width={640} height={320} style={{backgroundColor: 'black', marginTop: '50px', marginLeft: '50px'}} />
+			<div />
 		)
 	}
 
